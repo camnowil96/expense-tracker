2f80c7bfa6b1c7df916a3c69859c818901f4bfac
Cameron <camnowil96@gmail.com>
2024-12-11 16:23:54 -0500

    Added expense file to project files

---
+++ expenses.py
@@ -0,0 +1,17 @@
+# This is a program that tracks expenses
+# Add an expense
+# View expenses
+# Calculate total expenses
+# Save and view data
+
+from datetime import datetime
+#declare variables
+expenseCategory = input('Category:')
+expenseDate = input('Date: ')
+expenseCost = input('Cost: ')
+
+#function to add expenses
+
+#function to view expenses
+
+#function to calculate expenses
