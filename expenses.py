1367fd3348f3bebbe63a82ff7c35a8524233a4fb
Cameron <camnowil96@gmail.com>
2024-12-11 20:35:56 -0500

    Completed menu and add expense functions. Started view tracker function.

--- expenses.py
+++ expenses.py
@@ -7,7 +7,22 @@
 #import all the good stuff
 import json 
 from datetime import datetime
-
+#declare variables
+expense = {} 
+#Function to add a new expense
+def new_expense():
+    global confirm_expense 
+    num_entries = int(input('How many expenses would you like to log?: '))
+    for i in range(num_entries):
+        expenseDate = input('Enter expense date (mm-dd-yyyy): ')
+        expenseDate_new = datetime.strptime(expenseDate, '%m-%d-%Y')
+        expenseDate_new.strftime('%m-%d-%Y')
+        expenseCategory = input('Enter the expense category: ')
+        expenseCost = float(input('Enter the cost: $'))
+        expenseDescription = input('Enter a description: ')
+        expense[expenseDate] = expenseCategory.capitalize(), expenseCost, expenseDescription.capitalize()
+        print('New Expenses: ' , expense)
+        confirm_expense = input('Save entry? Select Y to continue, N to start over, or Exit to return to the main menu: ')
 #main menu function
 print('Hello there. Welcome to your Expense Tracker. ')
 def main_menu():   
@@ -16,36 +31,31 @@ def main_menu():
     #insert view tracker function
         print('View Tracker')
     elif menuOption == '2':
-    #insert Add Expense function
-        print('Add Expense')
+    #call new expense function
+        new_expense()
     else:
         x = 0
         while x < 1: 
             print("Sorry. I didn't catch that. \n")
             x += 1
         else:
-            main_menu()
-main_menu()  
-#Date function 
-#declare an empty list 
-expense = []
+            main_menu()  
+#call main menu function      
+main_menu()
 
-expense_date = input('Enter expense date: ')
-expense_date_new = datetime.strptime(expense_date, '%m-%d-%Y')
+#Call add expense function 
+if confirm_expense.lower() == 'y':
+    json_object = json.dumps(expense)
+    with open("tracker.json" , "a+") as f:
+        json.dump(json_object, f)
+elif confirm_expense.lower() == 'n':
+    print()
+    new_expense()
+else:
+    main_menu()
 
-#converts date to string and adds to expense list
-expense.append(expense_date_new.strftime('%m-%d-%Y'))
-#converts expense list into a JSON string 
-expenseReport = json.dumps(expense)
-print(expenseReport)
-
-#declare variables
-# expenseCategory = input('Category:')
-# expenseDate = input('Date: ')
-# expenseCost = input('Cost: ')
-
-#function to add expenses
-
-#function to view expenses
-
+#view tracker function
+def viewTracker():
+    json.loads(json)
+    json.dumps()
 #function to calculate expenses
